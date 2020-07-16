775f088f17aa0819d12669905d1f84bb0fee5c91
gm_desktop <gmesda23@gmail.com>
2020-07-15 22:59:48 -0400

    runnin list appends

--- top_50_stocks_marketcap.py
+++ top_50_stocks_marketcap.py
@@ -2,6 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
 r = requests.get('http://www.iweblists.com/us/commerce/MarketCapitalization.html', headers=headers)
+
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
+
+stock_tickers = soup.find_all('td', class_="body")
+
+li = []
+
+for stock_ticker in stock_tickers:
+    li.append(stock_ticker.text)
+    # print(stock_ticker.text, end='\n'*2)
+
+li2 = li[::3]
+print(li2)
